01206b5b85e9cb927922ff5ab1b7e749d2c9887e
Luiz Augusto Schneider da Silva <luizz.augustoll@hotmail.com>
2022-03-25 12:26:48 -0300

    put denoRestController in bin

--- bin/denoRestServerController.js
+++ bin/denoRestServerController.js
@@ -1,6 +1,6 @@
 //@ts-check
-import { parse } from "./deps/parse.js"
-import { createRestServerController } from "./RestServerController.js"
+import { parse } from "../deps/parse.js"
+import { createRestServerController } from "../RestServerController.js"
 
 const { port = "3000" } = parse(Deno.args)
 const ServerController = createRestServerController({ port: parseInt(port) })
