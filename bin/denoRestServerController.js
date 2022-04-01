9155bfd01e5bfe7d0a2ed6650614b5ab11a99fbe
Luiz Augusto Schneider da Silva <luizz.augustoll@hotmail.com>
2022-03-31 21:49:37 -0300

    add publicFolder in denoRestServerController.js

--- bin/denoRestServerController.js
+++ bin/denoRestServerController.js
@@ -2,6 +2,6 @@
 import { parse } from "../deps/parse.js"
 import { createRestServerController } from "../RestServerController.js"
 
-const { port = "3000" } = parse(Deno.args)
-const ServerController = createRestServerController({ port: parseInt(port) })
+const { port = "3000", publicFolder = "./public" } = parse(Deno.args)
+const ServerController = createRestServerController({ port: parseInt(port), publicFolder })
 ServerController.init()
